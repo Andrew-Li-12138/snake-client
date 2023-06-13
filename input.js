cfedc31dcb3111b7a729e26cc0b4b1c6648158b9
Andrew Li <130577102+Andrew-Li-12138@users.noreply.github.com>
2023-06-13 19:09:53 -0400

    implement special keys that send canned messages to the server

--- input.js
+++ input.js
@@ -2,39 +2,59 @@
 let connection;
 
 // setup interface to handle user input from stdin
-const setupInput = function (conn) {
+const setupInput = function(conn) {
   connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
   //specify what happens when "data:" is received from stdin
-  const handleUserInput = function (key) {
+  const handleUserInput = function(key) {
+    //handle ctrl + c
     if (key === '\u0003') {
-    process.exit();
-  }
-      if (key === '\u0077') {
-        connection.write("Move: up");}
-    
-        if (key === "\u0061") {
-          connection.write("Move: left")
-        }
-        
-        if (key === "\u0073") {
-          connection.write("Move: down")
-        }
-
-        if (key === "\u0064") {
-          connection.write("Move: right")
-        }
+      process.exit();
     }
+    // handle w,a,s,d as movement commands
+    if (key === '\u0077') {
+      connection.write("Move: up");
+    }
+    
+    if (key === "\u0061") {
+      connection.write("Move: left");
+    }
+        
+    if (key === "\u0073") {
+      connection.write("Move: down");
+    }
+
+    if (key === "\u0064") {
+      connection.write("Move: right");
+    }
+
+    //keys of space, g, b for sending canned messages
+
+    if (key === "\u0020") {
+      connection.write("Say: how's today?");
+    }
+        
+    if (key === "\u0067") {
+      connection.write("Say: good job!");
+    }
+
+    if (key === "\u0062") {
+      connection.write("Say: bye for now :)");
+    }
+
+
+  };
 
   stdin.on("data", handleUserInput);
   
   return stdin;
   
-  }
+};
 
-  module.exports = {
-    setupInput
-  }
+//export using ES6 object shorthand
+module.exports = {
+  setupInput
+};
