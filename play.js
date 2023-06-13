1f3800947dbe37d38d348e1ecc58f65eaaf4d573
Andrew Li <130577102+Andrew-Li-12138@users.noreply.github.com>
2023-06-13 17:05:30 -0400

    set up input interface and specify what happens when "data:" is received from stdin

--- play.js
+++ play.js
@@ -2,3 +2,25 @@ const { connect } = require("./client.js")
 
 console.log("Connecting ...");
 connect();
+
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  //specify what happens when "data:" is received from stdin
+  const handleUserInput = function (key) {
+    if (key === '\u0003') {
+    process.exit();
+  }
+}
+  // setup interface to handle user input from stdin
+  stdin.on("data", handleUserInput);
+  
+  return stdin;
+  
+  }
+
+ setupInput()
