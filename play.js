1a3acdf1c90a1439afecff12f4f7928c76e490eb
Andrew Li <130577102+Andrew-Li-12138@users.noreply.github.com>
2023-06-12 22:14:21 -0400

    imported client.js using ES6 Shorthand syntax

--- play.js
+++ play.js
@@ -1,22 +1,4 @@
-const net = require("net");
-
-//establish a connection with the game server
-const connect = function() {
-  const conn = net.createConnection({
-    host: "localhost",
-    port: 50541
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  // handle incoming data and console.log it for the player
-  conn.on("data", (data) => {
-    console.log("Game Over: ", data);
-  });
-
-  return conn;
-};
+const { connect } = require("./client.js")
 
 console.log("Connecting ...");
 connect();
