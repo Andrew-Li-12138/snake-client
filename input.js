04c63f6d10f7cdabbb590c563abf6e5329d9dbf2
Andrew Li <130577102+Andrew-Li-12138@users.noreply.github.com>
2023-06-14 00:42:50 -0400

    refactored handleUserInput function by using variables in constant.js and reduced repetitive logic

--- input.js
+++ input.js
@@ -1,6 +1,5 @@
-//Store the active TCP connection object
 let connection;
-
+const { keyMap } = require("./constants");
 // setup interface to handle user input from stdin
 const setupInput = function(conn) {
   connection = conn;
@@ -10,48 +9,22 @@ const setupInput = function(conn) {
   stdin.resume();
   //specify what happens when "data:" is received from stdin
   const handleUserInput = function(key) {
-    //handle ctrl + c
+
     if (key === '\u0003') {
       process.exit();
     }
-    // handle w,a,s,d as movement commands
-    if (key === '\u0077') {
-      connection.write("Move: up");
-    }
-    
-    if (key === "\u0061") {
-      connection.write("Move: left");
-    }
-        
-    if (key === "\u0073") {
-      connection.write("Move: down");
-    }
 
-    if (key === "\u0064") {
-      connection.write("Move: right");
+    if (keyMap[key]) {
+      let command = keyMap[key]
+     connection.write(command)
     }
-
-    //keys of space, g, b for sending canned messages
-
-    if (key === "\u0020") {
-      connection.write("Say: how's today?");
-    }
-        
-    if (key === "\u0067") {
-      connection.write("Say: good job!");
-    }
-
-    if (key === "\u0062") {
-      connection.write("Say: bye for now :)");
-    }
-
-
-  };
+  
+   }
 
   stdin.on("data", handleUserInput);
-  
+
   return stdin;
-  
+
 };
 
 //export using ES6 object shorthand
