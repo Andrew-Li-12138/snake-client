82a2e8169394501ad6257b0b181ccb0dfd598bed
Andrew Li <130577102+Andrew-Li-12138@users.noreply.github.com>
2023-06-14 00:43:41 -0400

    refactored line 5 and 6 by using variables in constant.js for better readability

--- client.js
+++ client.js
@@ -1,15 +1,16 @@
 //establish a connection with the game server
 const net = require("net");
+const { IP, PORT } = require("./constants");
 const connect = function() {
   const conn = net.createConnection({
-    host: "localhost",
-    port: 50541
+    host: IP,
+    port: PORT
   });
 
   //The "connect" event is triggered on a connection as soon as it is successfully established
   conn.on("connect", () => {
     console.log("Successfully connected to game server");
-    conn.write("Name: ALS");
+    conn.write("Name: AL");
    //setInterval(() => conn.write("Move: up"), 1000); - experiment 
    //setInterval(() => conn.write("Move: left"), 1000); -experiment
   })
