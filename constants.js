8487389ae2bc8dc0ee0524163b9eee2ffcee6114
Andrew Li <130577102+Andrew-Li-12138@users.noreply.github.com>
2023-06-14 00:40:42 -0400

    created constants.js to store constant values for other files easy access

---
+++ constants.js
@@ -0,0 +1,19 @@
+const IP = "localhost";
+const PORT = 50541;
+
+const keyMap = { "\u0077":  "Move: up",
+ "\u0061": "Move: left",
+  "\u0073": "Move: down",
+ "\u0064": "Move: right",
+"\u0020": "Say: how's today?",
+ "\u0067": "Say: good job!",
+"\u0062": "Say: bye for now :)" }
+
+
+
+module.exports = {
+  IP,
+  PORT,
+  keyMap,
+}
+
