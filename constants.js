2bc76ed0dc6f6235a6f94ef014e98523a7a04929
Andrew Li <130577102+Andrew-Li-12138@users.noreply.github.com>
2023-06-14 21:17:20 -0400

    added comments at line 4 and line 13

--- constants.js
+++ constants.js
@@ -1,6 +1,7 @@
 const IP = "localhost";
 const PORT = 50541;
 
+//key:value sets are stored in keyMap object for handleUserInput to easily access and avoid repetitive logics by using if statements
 const keyMap = { "\u0077":  "Move: up",
  "\u0061": "Move: left",
   "\u0073": "Move: down",
@@ -9,8 +10,7 @@ const keyMap = { "\u0077":  "Move: up",
  "\u0067": "Say: good job!",
 "\u0062": "Say: bye for now :)" }
 
-
-
+//export using ES6 object shorthand
 module.exports = {
   IP,
   PORT,
